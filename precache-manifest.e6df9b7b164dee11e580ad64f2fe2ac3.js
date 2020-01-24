beb69c86725de6cb9d8ca779cd7a248e18d507b3
mattzmyname <mwz62905@uga.edu>
2020-01-24 14:50:40 -0500

    Updates

---
+++ precache-manifest.e6df9b7b164dee11e580ad64f2fe2ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6090f4d0b99b5ac00e04b5c5c6aec82",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "56a80baa64ba51af405d",
+    "url": "/projects/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a61b5dc23f459c1789ec",
+    "url": "/projects/static/css/main.b3dfc00a.chunk.css"
+  },
+  {
+    "revision": "56a80baa64ba51af405d",
+    "url": "/projects/static/js/2.0c3e505a.chunk.js"
+  },
+  {
+    "revision": "a61b5dc23f459c1789ec",
+    "url": "/projects/static/js/main.b6902ab5.chunk.js"
+  },
+  {
+    "revision": "4cdf2632ba3fdf27d2fa",
+    "url": "/projects/static/js/runtime~main.f5f7314f.js"
+  }
+]);
